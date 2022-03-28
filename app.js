c6eaec2fe793c5bd54389dd5b0445f9bda560913
va9iff <va9iff@gmail.com>
2022-03-28 23:08:14 +0400

    initial

---
+++ app.js
@@ -0,0 +1,185 @@
+const grab = s => document.querySelector(s)
+
+function hexToHSL(H) {
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0)
+    h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  // return "hsl(" + h + "," + s + "%," + l + "%)";
+  return [h, s, l]
+}
+
+
+function hueOfHex(H) {
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0)
+    h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  // return "hsl(" + h + "," + s + "%," + l + "%)";
+  return h
+}
+
+
+function HSLToHex(h,s,l) {
+  s /= 100;
+  l /= 100;
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+      x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+      m = l - c/2,
+      r = 0,
+      g = 0, 
+      b = 0; 
+
+  if (0 <= h && h < 60) {
+    r = c; g = x; b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x; g = c; b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0; g = c; b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0; g = x; b = c;
+  } else if (240 <= h && h < 300) {
+    r = x; g = 0; b = c;
+  } else if (300 <= h && h < 360) {
+    r = c; g = 0; b = x;
+  }
+  // Having obtained RGB, convert channels to hex
+  r = Math.round((r + m) * 255).toString(16);
+  g = Math.round((g + m) * 255).toString(16);
+  b = Math.round((b + m) * 255).toString(16);
+
+  // Prepend 0s, if necessary
+  if (r.length == 1)
+    r = "0" + r;
+  if (g.length == 1)
+    g = "0" + g;
+  if (b.length == 1)
+    b = "0" + b;
+
+  return "#" + r + g + b;
+}
+
+// let exid = 3
+// let diapazon = 50
+
+let code = (id,hue)=>{
+	// if (hue<id) hue+=diapazon
+	let whole = parseInt(hue/diapazon)
+	let coded = whole*diapazon+id
+	return coded%360
+}
+
+let fun = ()=>{
+	diapazon = grab("#diapazon").value
+
+	hex = i.value
+	exid = +num.value%diapazon
+
+	let pHex = hex
+	let pHSL = hexToHSL(hex)
+
+	let pHue = pHSL[0]
+	let cHue = code(exid,pHue)
+
+	let cHSL = [...pHSL]
+	cHSL[0] = cHue
+
+	let cHex = HSLToHex(...cHSL)
+
+	o.value = cHex
+	ih.innerHTML = "input color hue: "+pHue+" -which means " + code(pHue,pHue)%diapazon
+	oh.innerHTML = "output color hue: "+cHue+ "-which means " + exid
+
+	console.log("\n",
+		"original 	hex",pHex,"\n",
+		"original 	hue",pHue,"\n",
+		"coded    	hue",cHue,"\n",
+		"coded    	hex",cHex,"\n",
+		)
+}
+
+let i = grab("#in")
+let o = grab("#out")
+let num = grab("#num")
+
+let ih = grab("#inHue")
+let oh = grab("#outHue")
+i.onchange = fun
+
+
+fun()
